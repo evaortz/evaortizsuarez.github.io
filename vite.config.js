d5a45a28fa24772406455684b1c0009614812e9f
evaortz <evaortz.art@gmail.com>
2025-04-24 11:05:07 +0200

    configure vit for github

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({server: 
-  {watch: {usePolling: true},},
+export default defineConfig({
+  base: '/portfolio/',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
-})
+});
+
